a511ff4fceb00761d7e5fd1a1468ee4969d0385d
Aryan Lnu <AryanLnu@fico.com>
2025-05-15 11:55:03 +0530

    tailwind added ad user registration lob created

---
+++ MajdooriCount/src/lib/lib-user-registration/README.md
@@ -0,0 +1,3 @@
+# lib-user-registration
+
+This library was generated with [Nx](https://nx.dev).
---
+++ MajdooriCount/src/lib/lib-user-registration/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { LibUserRegistrationComponent } from './lib-user-registration.component';
+
+export const libUserRegistrationRoutes: Route[] = [
+  { path: 'userRegister', component: LibUserRegistrationComponent },
+];
